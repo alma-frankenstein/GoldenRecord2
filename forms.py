3fa9a40cb444e7c293f511e9f13851fe769d20d4
Frankenstein <a.q.frankenstein@gmail.com>
2021-10-14 15:40:12 -0700

    delete all user stuff from forms

--- forms.py
+++ forms.py
@@ -2,22 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, FileField
 from wtforms.validators import DataRequired, EqualTo, ValidationError, Length
 
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember me')
-    submit = SubmitField('sign in')
-
-class RegistrationForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
-    password2 = PasswordField('repeat password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('register')
-
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username.data).first()
-    #     if user is not None:
-    #         raise ValidationError('please try a different username')
 
 class EmptyForm(FlaskForm):
     submit = SubmitField('submit')
@@ -26,10 +10,4 @@ class PostForm(FlaskForm):
     song = TextAreaField('song name', validators=[DataRequired(), Length(min=1, max=60)])
     artist = TextAreaField('artist name', validators=[DataRequired(), Length(min=1, max=60)])
     song_link = TextAreaField('link to song (optional)')
-    submit = SubmitField('submit')
-
-class EditProfileForm(FlaskForm):
-    file = FileField('File')
-    username = StringField('username')
-    about_me = TextAreaField('about me', validators=[Length(min=0, max=300)])
     submit = SubmitField('submit')
